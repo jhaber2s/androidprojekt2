7ebe9eeb17466bad12c235ed95496eba947a61e6
jens <jenshabermann89@yahoo.com>
2018-11-27 11:39:09 +0100

    test

--- app/src/main/java/com/example/jens/androidprojekt2/Connector.java
+++ app/src/main/java/com/example/jens/androidprojekt2/Connector.java
@@ -1,7 +1,22 @@
 package com.example.jens.androidprojekt2;
+
+import java.io.IOException;
+
+import lejos.nxt.comm.Bluetooth;
+import lejos.nxt.comm.NXTCommConnector;
+import lejos.nxt.comm.NXTConnection;
+import lejos.nxt.remote.NXTComm;
 import lejos.nxt.remote.RemoteNXT;
+
 public class Connector {
 
+
+    public void conect()
+
+    {
+
+
+    }
 
 
 }
